7c74426965d461787a14b443d01823b41c8b227b
Thiago Viccichi <viccichithiago@gmail.com>
2026-05-02 19:37:06 -0300

    Tarea: actualizar las dependencias en package.json
    - Se han añadido @iconify-json/devicon, @iconify-json/heroicons, @tailwindcss/vite, astro-icon y tailwindcss.
    - Se ha mantenido la versión existente de la dependencia astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+    icon(), // 2. Añade icon() aquí
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
